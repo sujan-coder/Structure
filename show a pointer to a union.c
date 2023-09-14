95add3560ac2fa06bd3f83358c4b03425edcf1ba
SUJAN K K <62798625+sujan-coder@users.noreply.github.com>
2023-09-14 13:32:48 +0530

    Create show a pointer to a union.c
    O/P:
    
    
     Pointer : Show a pointer to union :
    ----------------------------------------
     Jhon Mc Jhon Mc

---
+++ show a pointer to a union.c
@@ -0,0 +1,24 @@
+// Write a program in C to show a pointer to a union
+#include <stdio.h>
+union empAdd
+{
+char *ename;
+char stname[20];
+int pincode;
+};
+
+int main()
+{
+ 	printf("\n\n Pointer : Show a pointer to union :\n");
+	printf("----------------------------------------\n");
+    union empAdd employee,*pt;
+    employee.ename="Jhon Mc\0Donald";//assign  the string up to null character i.e. '\0'
+
+    pt=&employee;
+
+    printf(" %s %s\n\n",pt->ename,(*pt).ename);
+
+    return 0;
+}
+
+
